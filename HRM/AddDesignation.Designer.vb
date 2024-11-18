fa36e0784526480c827063962c02ac1bfd0bd3ee
orca <edoch06@gmail.com>
2024-11-18 10:48:09 +0300

    added adddept

---
+++ HRM/AddDesignation.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AddDesignation
+    Inherits DevComponents.DotNetBar.Office2007Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AddDesignation))
+        Me.StyleManager1 = New DevComponents.DotNetBar.StyleManager(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.CustomGroupBox2 = New HRM.CustomGroupBox
+        Me.btn_next = New System.Windows.Forms.Button
+        Me.btn_save = New System.Windows.Forms.Button
+        Me.btn_close = New System.Windows.Forms.Button
+        Me.CustomGroupBox1 = New HRM.CustomGroupBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.CustomPanel1 = New HRM.CustomPanel
+        Me.txt_descrip = New System.Windows.Forms.RichTextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txt_name = New System.Windows.Forms.TextBox
+        Me.Panel1.SuspendLayout()
+        Me.CustomGroupBox2.SuspendLayout()
+        Me.CustomGroupBox1.SuspendLayout()
+        Me.CustomPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StyleManager1
+        '
+        Me.StyleManager1.ManagerColorTint = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.StyleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Windows7Blue
+        Me.StyleManager1.MetroColorParameters = New DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer)), System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(115, Byte), Integer), CType(CType(70, Byte), Integer)))
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.CustomGroupBox2)
+        Me.Panel1.Controls.Add(Me.CustomGroupBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Padding = New System.Windows.Forms.Padding(5)
+        Me.Panel1.Size = New System.Drawing.Size(391, 199)
+        Me.Panel1.TabIndex = 1
+        '
+        'CustomGroupBox2
+        '
+        Me.CustomGroupBox2.BorderColor = System.Drawing.SystemColors.Control
+        Me.CustomGroupBox2.Controls.Add(Me.btn_next)
+        Me.CustomGroupBox2.Controls.Add(Me.btn_save)
+        Me.CustomGroupBox2.Controls.Add(Me.btn_close)
+        Me.CustomGroupBox2.Location = New System.Drawing.Point(8, 143)
+        Me.CustomGroupBox2.Name = "CustomGroupBox2"
+        Me.CustomGroupBox2.Size = New System.Drawing.Size(375, 48)
+        Me.CustomGroupBox2.TabIndex = 1
+        Me.CustomGroupBox2.TabStop = False
+        '
+        'btn_next
+        '
+        Me.btn_next.Location = New System.Drawing.Point(148, 13)
+        Me.btn_next.Name = "btn_next"
+        Me.btn_next.Size = New System.Drawing.Size(93, 23)
+        Me.btn_next.TabIndex = 4
+        Me.btn_next.Text = "Save && Next"
+        Me.btn_next.UseVisualStyleBackColor = True
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(45, 13)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(93, 23)
+        Me.btn_save.TabIndex = 3
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'btn_close
+        '
+        Me.btn_close.Location = New System.Drawing.Point(247, 13)
+        Me.btn_close.Name = "btn_close"
+        Me.btn_close.Size = New System.Drawing.Size(93, 23)
+        Me.btn_close.TabIndex = 5
+        Me.btn_close.Text = "Close"
+        Me.btn_close.UseVisualStyleBackColor = True
+        '
+        'CustomGroupBox1
+        '
+        Me.CustomGroupBox1.BorderColor = System.Drawing.SystemColors.Control
+        Me.CustomGroupBox1.Controls.Add(Me.Label3)
+        Me.CustomGroupBox1.Controls.Add(Me.CustomPanel1)
+        Me.CustomGroupBox1.Controls.Add(Me.Label1)
+        Me.CustomGroupBox1.Controls.Add(Me.txt_name)
+        Me.CustomGroupBox1.ForeColor = System.Drawing.SystemColors.Control
+        Me.CustomGroupBox1.Location = New System.Drawing.Point(8, 5)
+        Me.CustomGroupBox1.Name = "CustomGroupBox1"
+        Me.CustomGroupBox1.Size = New System.Drawing.Size(375, 132)
+        Me.CustomGroupBox1.TabIndex = 0
+        Me.CustomGroupBox1.TabStop = False
+        Me.CustomGroupBox1.Text = "Religion Details  "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 28
+        Me.Label3.Text = "Description"
+        '
+        'CustomPanel1
+        '
+        Me.CustomPanel1.BorderColor = System.Drawing.Color.Empty
+        Me.CustomPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CustomPanel1.BorderWidth = 0
+        Me.CustomPanel1.Controls.Add(Me.txt_descrip)
+        Me.CustomPanel1.Location = New System.Drawing.Point(82, 45)
+        Me.CustomPanel1.Name = "CustomPanel1"
+        Me.CustomPanel1.Size = New System.Drawing.Size(251, 74)
+        Me.CustomPanel1.TabIndex = 27
+        '
+        'txt_descrip
+        '
+        Me.txt_descrip.BackColor = System.Drawing.Color.White
+        Me.txt_descrip.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txt_descrip.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txt_descrip.Location = New System.Drawing.Point(0, 0)
+        Me.txt_descrip.Name = "txt_descrip"
+        Me.txt_descrip.Size = New System.Drawing.Size(249, 72)
+        Me.txt_descrip.TabIndex = 1
+        Me.txt_descrip.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 26
+        Me.Label1.Text = "Name *"
+        '
+        'txt_name
+        '
+        Me.txt_name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_name.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txt_name.Location = New System.Drawing.Point(82, 19)
+        Me.txt_name.Name = "txt_name"
+        Me.txt_name.Size = New System.Drawing.Size(251, 20)
+        Me.txt_name.TabIndex = 24
+        '
+        'AddReligion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(67, Byte), Integer), CType(CType(78, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(391, 199)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.EnableGlass = False
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "AddReligion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Add Religion"
+        Me.Panel1.ResumeLayout(False)
+        Me.CustomGroupBox2.ResumeLayout(False)
+        Me.CustomGroupBox1.ResumeLayout(False)
+        Me.CustomGroupBox1.PerformLayout()
+        Me.CustomPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents StyleManager1 As DevComponents.DotNetBar.StyleManager
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents CustomGroupBox2 As HRM.CustomGroupBox
+    Friend WithEvents btn_next As System.Windows.Forms.Button
+    Friend WithEvents btn_save As System.Windows.Forms.Button
+    Friend WithEvents btn_close As System.Windows.Forms.Button
+    Friend WithEvents CustomGroupBox1 As HRM.CustomGroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents CustomPanel1 As HRM.CustomPanel
+    Friend WithEvents txt_descrip As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_name As System.Windows.Forms.TextBox
+End Class
