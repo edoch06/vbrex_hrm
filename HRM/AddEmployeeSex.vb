9ed19e1669c4daa64e361dbb3c402f26facf8966
orca <edoch06@gmail.com>
2024-11-22 10:50:08 +0300

    added addEmployeeSex

---
+++ HRM/AddEmployeeSex.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports System.Media
+Public Class AddEmployeeSex
+    Private Const WM_SYSCOMMAND As Integer = &H112
+    Private Const SC_MAXIMIZE As Integer = &HF030
+    Private Const WM_NCLBUTTONDBLCLK As Integer = &HA3
+    Private Const HTCAPTION As Integer = &H2
+    Private widthint As Integer = 397
+    Private heightint As Integer = 227
+
+    Private classEmp As New ClassEmployee
+
+   
+End Class
