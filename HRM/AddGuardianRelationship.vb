063517834a17b6d1f5bc2539dcc2fe439b97a987
orca <edoch06@gmail.com>
2025-01-13 11:16:59 +0300

    added addGuardianRelation

---
+++ HRM/AddGuardianRelationship.vb
@@ -0,0 +1,13 @@
+﻿
+Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports System.Media
+Public Class AddGuardianRelationship
+   
+End Class
